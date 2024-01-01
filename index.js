f14383a13a05254570fba6ee60019e8187344e16
evanlin917 <evanlin1724@gmail.com>
2023-12-31 21:10:21 -0500

    Challenge 4 of 7

--- index.js
+++ index.js
@@ -69,19 +69,31 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce( (accContributors, game) => {
+    return accContributors + game.backers;
+}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `<p>${totalContributions.toLocaleString("en-US")}</p>`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+const totalRaised = GAMES_JSON.reduce( (accRaised, game) => {
+    return accRaised + game.pledged;
+}, 0);
 
+// set inner HTML using template literal
+raisedCard.innerHTML = `<p>${totalRaised.toLocaleString("en-US")}</p>`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+const totalGames = GAMES_JSON.reduce( (accGames, game) => {
+    return accGames + 1;
+}, 0);
+
+gamesCard.innerHTML = `<p>${totalGames.toLocaleString("en-US")}</p>`;
 
 
 /*************************************************************************************
